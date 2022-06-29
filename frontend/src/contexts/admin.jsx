b81440c0bdbf4fc82a44d0685dbb421cc00b82c5
JbSense <dev.jvxavier@gmail.com>
2022-06-29 20:09:06 -0300

    Api adicionada

--- frontend/src/contexts/admin.jsx
+++ frontend/src/contexts/admin.jsx
@@ -5,7 +5,9 @@ export const AdminContext = createContext()
 // eslint-disable-next-line react/prop-types
 export const AdminProvider = ({ children }) => {
   // eslint-disable-next-line no-unused-vars
-  const [admin, setAdmin] = useState(null)
+  const [admin, setAdmin] = useState({
+    name: 'Sukuna Ryomena'
+  })
 
   // setAdmin({ name: 'Sukuna Ryomena' })
 
--- frontend/src/pages/public/Home/Home.jsx
+++ frontend/src/pages/public/Home/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AdminContext } from '../../../contexts/admin'
 
 const Home = () => {
+  const { admin } = useContext(AdminContext)
+
   return (
     <div className='Home'>
       <h1>Home</h1>
+      <h2>{admin.name}</h2>
     </div>
   )
 }
